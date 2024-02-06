c8cf03f42f054d57ed1c42b81a121dbc8c1ef0c0
Aashu Kumar <94188500+Rask6723@users.noreply.github.com>
2024-02-06 16:24:38 +0000

    container config

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId=1234
+let accountEmail="aashu0123kumar@gmail.com"
+var accountPassword="12345"
+accountCity="kolkata"
+
+//accountId=343
+console.table([accountId,accountEmail,accountPassword,accountCity])
+accountEmail="deepak6906kumar@gmail.com"
+accountPassword="345"
+accountCity="bettiah"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
