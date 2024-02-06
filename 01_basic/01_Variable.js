19c1329a66ba786a0f4b15874a64f43aaefc20ea
Aashu Kumar <94188500+Rask6723@users.noreply.github.com>
2024-02-06 16:33:40 +0000

    container config

--- 01_basic/01_Variable.js
+++ 01_basic/01_Variable.js
@@ -2,7 +2,9 @@ const accountId=1234
 let accountEmail="aashu0123kumar@gmail.com"
 var accountPassword="12345"
 accountCity="kolkata"
-
+/*
+prefer not use var keyword b/c of issue in scope and funcational scope;
+*/
 //accountId=343
 console.table([accountId,accountEmail,accountPassword,accountCity])
 accountEmail="deepak6906kumar@gmail.com"
